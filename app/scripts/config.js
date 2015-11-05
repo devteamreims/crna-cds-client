5cd9097d659cc38784369f571df4c03efde49483
Benjamin Beret <benjamin.beret@gmail.com>
2015-11-05 21:30:50 +0100

    Remove dark theme (still a handful of bugs)

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -16,12 +16,12 @@ function applyTheme($mdThemingProvider) {
   // We should define a dark theme here, but this is not working consistently with ngMaterial
   $mdThemingProvider.theme('default')
     .primaryPalette('indigo')
-    .accentPalette('deep-orange').dark()
+    .accentPalette('deep-orange')
     ;
   $mdThemingProvider.theme('success-warning')
     .primaryPalette('green')
     .accentPalette('orange', {
       'default': '800'
-    }).dark()
+    })
     ;
 }
