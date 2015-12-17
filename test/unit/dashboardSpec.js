2666f16b3d37e44ac1867f3444c8bde2890da72a
Benjamin Beret <benjamin.beret@gmail.com>
2015-12-17 17:17:09 +0100

    Add a stub backend reponse to make test pass

--- test/unit/dashboardSpec.js
+++ test/unit/dashboardSpec.js
@@ -7,10 +7,17 @@ describe('Dashboard', function() {
 
   // Store references to $rootScope and $compile
   // so they are available to all tests in this describe block
-  beforeEach(inject(function(_$compile_, _$rootScope_){
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$httpBackend_, _cdsBackendUrl_){
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $compile = _$compile_;
     $rootScope = _$rootScope_;
+    $httpBackend = _$httpBackend_;
+    $httpBackend.when('GET', _cdsBackendUrl_ + '/positions')
+          .respond([
+            {id: 30, disabled: false, sectors: ['UR', 'XR']},
+            {name: 'UR', elementarySectors: ['UR']},
+            {name: '5R', elementarySectors: ['UR', 'XR', 'KR', 'YR', 'HR']}
+          ]);
   }));
 
   it('should show ctrlroom buttons', function() {
